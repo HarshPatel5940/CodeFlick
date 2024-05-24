e6731d58391702342cad72b1bfbc3c34a45879d3
Harsh Patel <harshnpatel9@gmail.com>
2024-05-24 21:21:54 +0530

    chore: add simple contact form

--- main.go
+++ main.go
@@ -26,16 +26,51 @@ type Count struct {
 	Count int
 }
 
+type Person struct {
+	Name  string
+	Email string
+}
+
+type H = map[string]interface{}
+
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type contacts = []Contact
+
+type Data struct {
+	Contacts contacts
+}
+
+func newContact(name string, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("harsh", "test@gmail.com"),
+			newContact("harsh-2", "test-2@gmail.com"),
+		},
+	}
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
+	e.Renderer = newTemplate()
 
 	count := &Count{Count: 0}
-	e.Renderer = newTemplate()
+	data := newData()
 
 	e.GET("/", func(c echo.Context) error {
 
-		return c.Render(200, "index", map[string]interface{}{
+		return c.Render(200, "index", H{
 			"title": "Hello World",
 			"Count": count.Count,
 		})
@@ -43,9 +78,24 @@ func main() {
 
 	e.POST("/api/increment", func(c echo.Context) error {
 		count.Count++
-		return c.Render(200, "counter", map[string]interface{}{
+		return c.Render(200, "counter", H{
 			"Count": count.Count,
 		})
+	})
+
+	e.GET("/contacts", func(c echo.Context) error {
+		return c.Render(200, "contact-index", data)
+	})
+
+	e.POST("/api/contacts", func(c echo.Context) error {
+		name := c.FormValue("Name")
+		email := c.FormValue("Email")
+		newContact := newContact(name, email)
+		data.Contacts = append(data.Contacts, newContact)
+
+		e.Logger.Info(data.Contacts)
+
+		return c.Render(200, "contacts", data)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
