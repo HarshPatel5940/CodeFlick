5f4dcab921925049559190916bc52d4e78e6586d
Harsh Patel <harshnpatel9@gmail.com>
2024-07-31 17:50:17 +0530

    refact: move to use ulid

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID           int    `json:"id" db:"id"`
+	ID           string `json:"id" db:"id"`
 	Name         string `json:"name" db:"name"`
 	Email        string `json:"email" db:"email" `
 	AuthProvider string `json:"authProvider" db:"auth_provider"`
